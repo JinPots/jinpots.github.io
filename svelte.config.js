a226898b23cf1444c5b11a550382e98a120b0d99
JinPots <shot27012@gmail.com>
2025-11-07 16:02:10 +0700

    cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,23 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from "@sveltejs/adapter-cloudflare";
 
-export default {
-	kit: {
-		adapter: adapter()
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      // See below for an explanation of these options
+      config: undefined,
+      platformProxy: {
+        configPath: undefined,
+        environment: undefined,
+        persist: undefined,
+      },
+      fallback: "plaintext",
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
+    }),
+  },
 };
+
+export default config;
